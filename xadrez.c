b1ffe31365c5312141a5ac634d077ce79443255c
Bruno <brunovonassis@gmail.com>
2025-05-31 18:01:41 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -13,12 +13,21 @@ int main() {
         printf("Esquerda\n"); //movimento da rainha
         i--;
     }
-    
+    {
     i = 0;
     do {
         printf("Cima e direita\n"); //movimento do bispo em diagonl
         i++;
     } while (i <= 7);
+    }
+    i = 3;
+    while (i > 0) {
+        i--;
+        for (int j = 0; j < 2; j++) {
+            printf("Esquerda\n"); //movimento do cavalo em duas casas para a esquerda 
+        }
+        printf("Baixo\n"); //movimento do cavalo em uma casa para baixo
+    }
 
     return 0;
     
